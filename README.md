2700665bc295dd171b5605afcfa533509ad0a6d1
Michael Gallagher <michael.gallagher@teamwork.com>
2018-02-19 08:49:06 -0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # fabric.js-browser
 Fabric.js reworked to support Webpack bundling
+
+This project has a specific Fabric.js version checked in, which as part of its build, will
+modify to work currently under webpack.
+
+The new dependency (under `/dist`) will not add to the global scope, and is in fact a builder
+function.
+
+### Build
+
+`npm run build`
+
+### Usage
+
+```
+require(['fabric.js-browser'], function(fabricBuilder) {
+	var fabric = fabricBuilder();
+	...
+	var cvs = new fabric.Canvas()
+	...
+});
---
+++ src/build.js
@@ -0,0 +1,16 @@
+const { readFileSync, writeFileSync } = require('fs');
+const { resolve } = require('path');
+
+/**
+ * Wraps the Fabric dependency in a builder function, means it will be built during the browser runtime
+ * which is what it needs. It also removed the global reference.
+ */
+const contents = readFileSync(resolve(__dirname, '../lib/fabric.js/dist/all.min.js'));
+
+// Replace nodejs require statements - they will never be run anyway
+const webpackSafe = contents.toString().replace(/require\('[a-z]+'\)/g, '{}').replace(/require\("[a-z]+"\)/g, '{}');
+
+const wrapped = 'var builder = function() {' + webpackSafe + 'delete this.fabric; return fabric; };' +
+  'var built = null; module.exports = () => built || (built = builder());'
+
+writeFileSync(resolve(__dirname, '../dist/browser.min.js'), wrapped);
